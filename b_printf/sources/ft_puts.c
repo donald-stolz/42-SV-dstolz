c824e488acffbf73f8eabae10f37858994964035
Donald Stolz <donaldtstolz@gmail.com>
2018-09-26 09:36:48 -0700

    Update for NULL string case

--- b_printf/sources/ft_puts.c
+++ b_printf/sources/ft_puts.c
@@ -6,12 +6,11 @@
 /*   By: dstolz <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/13 09:57:06 by dstolz            #+#    #+#             */
-/*   Updated: 2018/09/19 14:27:42 by dstolz           ###   ########.fr       */
+/*   Updated: 2018/09/25 18:45:15 by dstolz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libftprintf.h"
-#include <stdio.h>
 
 int	ft_puts(char *s)
 {
@@ -25,7 +24,12 @@ int	ft_puts(char *s)
 
 int	ft_putstr(va_list arg)
 {
-	return (ft_puts(va_arg(arg, char *)));
+	char *ptr;
+
+	ptr = va_arg(arg, char *);
+	if (ptr == NULL)
+		return (ft_puts("(null)"));
+	return (ft_puts(ptr));
 }
 
 int	ft_putarg(va_list arg)
