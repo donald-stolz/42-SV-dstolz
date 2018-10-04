327d8aeecbb1f677beba00f34866a74b08fa6b12
Donald Stolz <donaldtstolz@gmail.com>
2018-10-04 09:14:01 -0700

    Uploading /lst files
    Normed, not debugged/tested

---
+++ libft/lst/ft_lstdel.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdel.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dstolz <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/03 16:37:53 by dstolz            #+#    #+#             */
+/*   Updated: 2018/10/03 16:37:55 by dstolz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
+{
+	t_list *lst;
+	t_list *tmp;
+
+	lst = *alst;
+	while (lst)
+	{
+		tmp = lst->next;
+		del((*lst)->content, (*lst)->content_size);
+		free(*lst);
+		lst = tmp;
+	}
+	*alst = NULL;
+}
