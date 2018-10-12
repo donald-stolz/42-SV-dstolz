8837f5050a04084386d81d977abe91b197bfc6bc
Donald Stolz <donaldtstolz@gmail.com>
2018-10-12 08:41:14 -0700

    Updating ft_putstr

--- libft/print/ft_putstr_fd.c
+++ libft/print/ft_putstr_fd.c
@@ -14,7 +14,13 @@
 
 void	ft_putstr_fd(const char *s, int fd)
 {
+	size_t i;
+
+	i = 0;
 	if (s)
-		while (*s)
-			ft_putchar_fd(*s++, fd);
+	{
+		while (*(s + i))
+			i++;
+		write(fd, s, i);
+	}
 }
