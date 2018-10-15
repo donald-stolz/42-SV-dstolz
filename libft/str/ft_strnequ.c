a059f29ebd5f4a56b1a285257a058f0bcfe86a58
Donald Stolz <donaldtstolz@gmail.com>
2018-10-15 11:12:33 -0700

    Debugged ft_strnequ

--- libft/str/ft_strnequ.c
+++ libft/str/ft_strnequ.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft.h"
+#include "libft.h"
 
 int	ft_strnequ(char const *s1, char const *s2, size_t n)
 {
@@ -19,7 +19,9 @@ int	ft_strnequ(char const *s1, char const *s2, size_t n)
 	i = 0;
 	if (!s1 || !s2)
 		return (0);
-	while (*(s1 + i) && (*(s1 + i) == *(s2 + i)) && i < n)
+	if (n == 0)
+		return (1);
+	while (*(s1 + i) && (*(s1 + i) == *(s2 + i)) && i < (n - 1))
 		i++;
 	if (*((unsigned char *)s1 + i) - *((unsigned char *)s2 + i) != 0)
 		return (0);
