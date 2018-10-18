757e3a54488638e441c43226790c181a2aa635bd
Donald Stolz <donaldtstolz@gmail.com>
2018-10-18 13:43:44 -0700

    Updated memmove

--- libft/mem/ft_memmove.c
+++ libft/mem/ft_memmove.c
@@ -10,24 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft.h"
+#include "libft.h"
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	size_t i;
-
-	i = 0;
-	if (*((unsigned char *)src) < *((unsigned char *)dst))
+	if (!((int *)dst > (int *)src && (int *)src + len > (int *)dst))
+		ft_memcpy(dst, src, len);
+	else
+	{
 		while (len > 0)
 		{
 			len--;
-			*((unsigned char *)dst + len) = *((unsigned char *)src + len);
+			*((char *)dst + len) = *((char *)src + len);
 		}
-	else
-		while (i < len)
-		{
-			*((unsigned char *)dst + i) = *((unsigned char *)src + i);
-			i++;
-		}
+	}
 	return (dst);
 }
