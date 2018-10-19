14559e93cfc9c39de14ea87edbe61cec3d375ae0
Donald Stolz <donaldtstolz@gmail.com>
2018-10-19 08:19:02 -0700

    Update libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -90,5 +90,5 @@ void			ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void			ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list			*ft_lstmap(t_list *lst, t_list *(f)(t_list *elem));
 t_list			*ft_lstnew(void const *content, size_t content_size);
-void			ft_revlist(t_list *list);
+void			ft_lstrev(t_list *list);
 #endif
