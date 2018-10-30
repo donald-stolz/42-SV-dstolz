655b6a387db360e6540372360396f8446c5dfc7e
Donald Stolz <donaldtstolz@gmail.com>
2018-10-30 10:49:40 -0700

    Fixed all but 1 leak

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -13,6 +13,46 @@
 #include "get_next_line.h"
 #include "stdio.h"
 
+static t_list	*get_list(t_list **file, int fd)
+{
+	t_list *tmp;
+
+	tmp = *file;
+	while (tmp)
+	{
+		if ((int)tmp->content_size == fd)
+			return (tmp);
+		tmp = tmp->next;
+	}
+	tmp = ft_lstnew(ft_strnew(1), fd); //MEMCHECK(1)
+	ft_lstadd(file, tmp);
+	return (*file);
+}
+
+static int		read_file(char **str, const int fd)
+{
+	char	buf[BUFF_SIZE + 1];
+	ssize_t	bytes_read;
+	char	*tmp;
+
+	
+	tmp = *str;
+	if (ft_strlen(tmp) > 1)
+		*str = ft_strchr((const char *)tmp, '\n') + 1;
+	bytes_read = 0;
+	while ((bytes_read = read(fd, &buf, BUFF_SIZE)) > 0)
+	{
+		buf[bytes_read] = '\0';
+		MALLCHECK((*str = ft_strjoin((const char *)(*str), (const char *)&buf)));
+		free(tmp);
+		if (ft_strchr((const char *)&buf, '\n'))
+			break;
+		else
+			tmp = *str;
+	}
+	return (bytes_read);
+}
+
 static size_t	copy_line(char **line, const char *src)
 {
 	char	*str;
@@ -31,22 +71,6 @@ static size_t	copy_line(char **line, const char *src)
 	return (i);
 }
 
-static t_list	*get_list(t_list **file, int fd)
-{
-	t_list *tmp;
-
-	tmp = *file;
-	while (tmp)
-	{
-		if ((int)tmp->content_size == fd)
-			return (tmp);
-		tmp = tmp->next;
-	}
-	tmp = ft_lstnew(ft_strnew(1), fd); //MEMCHECK(1)
-	ft_lstadd(file, tmp);
-	return (*file);
-}
-
 int				get_next_line(const int fd, char **line)
 {
 	static t_list	*buffers;
@@ -57,20 +81,16 @@ int				get_next_line(const int fd, char **line)
 	if (fd < 0 || line == NULL || (-1 == read(fd, buf, 0)))
 		return (-1);
 	curr = get_list(&buffers, fd); //MEMCHECK(1)
-	while ((bytes_read = read(fd, &buf, BUFF_SIZE)) > 0)
-	{
-		buf[bytes_read] = '\0';
-		MALLCHECK((curr->content = ft_strjoin((const char *)curr->content,
-						(const char *)&buf))); //MEMCHECK(2)
-		if (ft_strchr((const char *)&buf, '\n'))
-			break ;
-	}
+	bytes_read = read_file((char **)(&curr->content), fd);
 	if (bytes_read < BUFF_SIZE && !ft_strlen((const char *)curr->content))
+	{
+		// Free list
 		return (0);
+	}
 	MALLCHECK((*line = ft_strnew(1)));
 	bytes_read = copy_line(line, (const char *)curr->content);
-	((size_t)bytes_read < ft_strlen((const char *)curr->content))
-		? curr->content += (bytes_read + 1)
-		: ft_strclr((char *)curr->content);
+
+	if (!((size_t)bytes_read < ft_strlen((const char *)curr->content)))
+		ft_strclr((char *)curr->content);
 	return (1);
 }
