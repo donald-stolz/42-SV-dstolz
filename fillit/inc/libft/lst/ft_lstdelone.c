2de695117b68b1a4061dd45fead0aea6e26b496b
Donald Stolz <donaldtstolz@gmail.com>
2018-11-06 17:30:01 -0800

    Updated make and directory structure

--- fillit/inc/libft/lst/ft_lstdelone.c
+++ fillit/inc/libft/lst/ft_lstdelone.c
@@ -1,13 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dstolz <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/04 11:18:03 by dstolz            #+#    #+#             */
-/*   Updated: 2018/11/04 11:18:05 by dstolz           ###   ########.fr       */
+/*   Created: 2018/10/03 16:38:08 by dstolz            #+#    #+#             */
+/*   Updated: 2018/10/03 16:38:11 by dstolz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../libft.h"
 
+void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
+{
+	if (alst)
+	{
+		del((*alst)->content, (*alst)->content_size);
+		free(*alst);
+		*alst = NULL;
+	}
+}
---
+++ fillit/inc/libft/str/ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dstolz <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/30 15:47:41 by dstolz            #+#    #+#             */
+/*   Updated: 2018/09/30 15:47:47 by dstolz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t	i;
+	size_t	size;
+	char	*str;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	size = ft_strlen(s);
+	MALLCHECK((str = (char *)malloc((size + 1) * sizeof(*str))));
+	while (i < size)
+	{
+		*(str + i) = f(i, *((char *)s + i));
+		i++;
+	}
+	*(str + i) = '\0';
+	return (str);
+}
