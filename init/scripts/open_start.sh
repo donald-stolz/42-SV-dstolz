84a0a306c4a736dc38c8af49d1a9319879919c56
Donald Stolz <donaldtstolz@gmail.com>
2018-12-23 09:59:18 -0800

    Update open_start.sh

--- init/scripts/open_start.sh
+++ init/scripts/open_start.sh
@@ -1,5 +1,6 @@
 osascript -e "tell application \"Google Chrome\"" -e "activate" -e "tell application \"System Events\"" -e "keystroke \"f\" using {control down, command down}" -e "end tell" -e "end tell"
 open -a "Google Chrome" https://tomato-timer.com/
-open -a "Google Chrome" https://youtube.com/
+# open -a "Google Chrome" https://youtube.com/
 open -a "Google Chrome" https://intra.42.fr/
+osascript -e "tell application \"Spotify\"" -e "activate" -e "make new window" -e "tell application \"System Events\"" -e "keystroke \"f\" using {control down, command down}" -e "end tell" -e "end tell"
 osascript -e "tell application \"Slack\"" -e "activate" -e "make new window" -e "tell application \"System Events\"" -e "keystroke \"f\" using {control down, command down}" -e "end tell" -e "end tell"
