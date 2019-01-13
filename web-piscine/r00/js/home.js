b56fd6e9a6e4991ebe8f755381db06d59d02267d
donald-stolz <donaldtstolz@gmail.com>
2019-01-12 21:54:20 -0800

    Adding rush 00

---
+++ web-piscine/r00/js/home.js
@@ -0,0 +1,11 @@
+function sortCruises(event) {}
+
+function sortExcursions(event) {}
+
+function sortSpas(event) {}
+
+function minusCount(counterId) {}
+
+function plusCount(counterId) {}
+
+function addToCart(itemId, counterId) {}
