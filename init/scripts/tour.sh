790de7a5f880bb2ab2e0c152c254077501bfeea0
donald-stolz <donaldtstolz@gmail.com>
2019-02-03 09:37:00 -0800

    Added script for presenting on tours

---
+++ init/scripts/tour.sh
@@ -0,0 +1,3 @@
+osascript -e "tell application \"Google Chrome\"" -e "activate" -e "tell application \"System Events\"" -e "keystroke \"f\" using {control down, command down}" -e "end tell" -e "end tell"
+open -a "Google Chrome" https://profile.intra.42.fr/
+open -a "Google Chrome" https://projects.intra.42.fr/projects/graph
