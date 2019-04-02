cbc83d2b04a8f87d4c6c1950784483aa758832df
Donald Stolz <donaldtstolz@gmail.com>
2019-04-02 07:26:32 -0700

    Add check_mate; Update .gitignore

---
+++ exams/C_Exam_Alone_In_The_Dark-Beginner/level-4/check_mate/check_mate.c
@@ -0,0 +1,147 @@
+
+// Test from King
+#include <unistd.h>
+
+void get_king(char **map, int *krow, int *kcol)
+{
+	*krow = 0;
+	*kcol = 0;
+
+	while(map[*krow])
+	{
+		while(map[*krow][*kcol])
+		{
+			if (map[*krow][*kcol] == 'K')
+				return;
+			*kcol++;
+		}
+		*krow++;
+		*kcol = 0;
+	}
+	*krow = -1;
+	*kcol = -1;
+}
+
+int check_ortho(char **map, int krow, int kcol)
+{
+	int row = krow;
+	int col = kcol;
+
+	while(row > 0){
+		row--;
+		if (map[row][col] == 'Q' || map[row][col] == 'R')
+			return (1);
+		else if (map[row][col] == 'P' || map[row][col] == 'B')
+			break;
+	}
+	while(col > 0){
+		col--;
+		if (map[row][col] == 'Q' || map[row][col] == 'R')
+			return (1);
+		else if (map[row][col] == 'P' || map[row][col] == 'B')
+			break;
+	}
+	row = krow;
+	col = kcol;
+	while (map[row + 1][col])
+	{
+		row++;
+		if (map[row][col] == 'Q' || map[row][col] == 'R')
+			return (1);
+		else if (map[row][col] == 'P' || map[row][col] == 'B')
+			break;
+	}
+	while (map[row][col + 1])
+	{
+		col++;
+		if (map[row][col] == 'Q' || map[row][col] == 'R')
+			return (1);
+		else if (map[row][col] == 'P' || map[row][col] == 'B')
+			break;
+	}
+}
+
+int	check_diagonal(char **map, int krow, int kcol)
+{
+	int row = krow;
+	int col = kcol;
+
+	while(row > 0 && col > 0 )
+	{
+		row--;
+		col--;
+		if (map[row][col] == 'B' || map[row][col] == 'Q')
+			return (1);
+		else if (map[row][col] == 'P' || map[row][col] == 'R')
+			break;
+	}
+	row = krow;
+	col = kcol;
+	while (map[row + 1][col + 1])
+	{
+		row++;
+		col++;
+		if (map[row][col] == 'B' || map[row][col] == 'Q')
+			return (1);
+		else if (map[row][col] == 'P' || map[row][col] == 'R')
+		{
+			if (row == krow + 1)
+				return (1);
+			else
+				break;
+		}
+	}
+	row = krow;
+	col = kcol;
+	while (map[row + 1][col - 1] && col > 0)
+	{
+		row++;
+		col--;
+		if (map[row][col] == 'B' || map[row][col] == 'Q')
+			return (1);
+		else if (map[row][col] == 'P' || map[row][col] == 'R')
+		{
+			if (row == krow + 1)
+				return(1);
+			else
+				break;
+		}
+	}
+	row = krow;
+	col = kcol;
+	while (map[row - 1][col + 1] && row > 0)
+	{
+		row--;
+		col++;
+		if (map[row][col] == 'B' || map[row][col] == 'Q')
+			return (1);
+		else if (map[row][col] == 'P' || map[row][col] == 'R')
+			break;
+	}
+	
+	
+}
+
+int main(int argc, char **argv)
+{
+	int *krow;
+	int *kcol;
+	char **map;
+	if (argc > 1)
+	{
+		// Parse input into 2D char array
+		map = &argv[1];
+		// Find pos of king
+		get_king(map, krow, kcol);
+		// Check horizontal & vertical (Orthogonal)
+		if(check_ortho(map, &krow, &kcol))
+			write(1, "Success", 7);
+		// Check diagonal
+		else if(check_diagonal(map, &krow, &kcol))
+			write(1, "Success", 7);
+		else
+			write(1, "Fail", 4);
+	}
+	write(1, "\n", 1);
+	return 0;
+}
